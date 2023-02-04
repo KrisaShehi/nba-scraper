2a064efe8106b29d7377834c9d505541553ca6af
a <a@MacBook-Pro-di-a.local>
2023-02-04 22:23:08 +0100

    vercel update 5

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const { allPlayers } = require('./controllers/players');
 app.use(express.json());
 const port = process.env.PORT || 4000;
 
+app.get('/', (req, res) => {
+  res.send('it works!');
+});
+
 app.get('/api/nba', (req, res) => {
   res.send('Hello World')
 });
